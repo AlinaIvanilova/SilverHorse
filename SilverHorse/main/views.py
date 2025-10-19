5ea8697a4d2447c6e9aa62837af3a458b013c5b8
AlinaIvanilova <alinatelf@gmail.com>
2025-10-19 12:58:11 +0300

    3.1 fix btn. All the btn work good

--- SilverHorse/main/views.py
+++ SilverHorse/main/views.py
@@ -1,4 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 def home(request):
     return render(request, 'main/home.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {'error': 'Невірний логін або пароль'})
+    return render(request, 'main/login.html')
+
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 != password2:
+            return render(request, 'main/register.html', {'error': 'Паролі не співпадають'})
+
+        if User.objects.filter(username=username).exists():
+            return render(request, 'main/register.html', {'error': 'Користувач з таким ім’ям вже існує'})
+
+        # Створюємо нового користувача
+        user = User.objects.create_user(username=username, password=password1)
+        return redirect('login')
+
+    return render(request, 'main/register.html')
